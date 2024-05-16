b7996455bed6138e7ddc801ff0cad25f332f511e
Nathalie Bonin <nbonin@umd.edu>
2024-05-16 09:22:58 -0400

    Final results

---
+++ quantify_simulated_data_both.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH --job-name=test
+#SBATCH --output=test.out.%j
+#SBATCH --error=test.err.%j
+#SBATCH --time=01-00:00:00
+#SBATCH --account=cbcb
+#SBATCH --qos=huge-long
+#SBATCH --nodes=32
+#SBATCH --ntasks=32
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=5000
+#SBATCH --partition=cbcb
+
+for fn in simulated_data/out_{1..12};
+do
+samp=`basename ${fn}`
+
+
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 1 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_01_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_01_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1e-5 --validateMappings -o quants/${samp}_01_quant_both_10^-5"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 2 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_02_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_02_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1e-5 --validateMappings -o quants/${samp}_02_quant_both_10^-5"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 1 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_01_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_01_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1e-4 --validateMappings -o quants/${samp}_01_quant_both_10^-4"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 2 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_02_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_02_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1e-4 --validateMappings -o quants/${samp}_02_quant_both_10^-4"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 1 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_01_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_01_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1e-3 --validateMappings -o quants/${samp}_01_quant_both_10^-3"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 2 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_02_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_02_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1e-3 --validateMappings -o quants/${samp}_02_quant_both_10^-3"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 1 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_01_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_01_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1e-2 --validateMappings -o quants/${samp}_01_quant_both_10^-2"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 2 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_02_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_02_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1e-2 --validateMappings -o quants/${samp}_02_quant_both_10^-2"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 1 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_01_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_01_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1e-1 --validateMappings -o quants/${samp}_01_quant_both_10^-1"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 2 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_02_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_02_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1e-1 --validateMappings -o quants/${samp}_02_quant_both_10^-1"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 1 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_01_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_01_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1 --validateMappings -o quants/${samp}_01_quant_both_10^0"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 2 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_02_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_02_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 1 --validateMappings -o quants/${samp}_02_quant_both_10^0"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 1 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_01_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_01_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 10 --validateMappings -o quants/${samp}_01_quant_both_10^1"&
+srun --time=01-00:00:00 --account=cbcb --qos=huge-long --nodes=1 --ntasks=1 --partition=cbcb --mem=5000 bash -c "hostname; echo \"Processing sample 2 of 2 in ${samp} with both\"; ./bin/salmon quant -i transcriptome/human_index -l IU -1 ${fn}/${samp}/sample_02_1_shuffled.fa.gz -2 ${fn}/${samp}/sample_02_2_shuffled.fa.gz -p 6 --gcBias --useBoth --vbPrior 10 --validateMappings -o quants/${samp}_02_quant_both_10^1"&
+
+done
+
+wait
--- transcriptome_index_build.sh
+++ transcriptome_index_build.sh
@@ -2,6 +2,6 @@
 
 mkdir transcriptome
 cd transcriptome
-curl https://ftp.ensembl.org/pub/release-111/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz -o human.fa.gz
+curl https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_28/GRCh37_mapping/gencode.v28lift37.transcripts.fa.gz -o human.fa.gz
 cd ../
 ./bin/salmon index -t transcriptome/human.fa.gz -i transcriptome/human_index
